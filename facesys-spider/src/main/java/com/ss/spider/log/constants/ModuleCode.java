cd4d3230c84204810b173ef319d6437968e58de1
chaos6666 <13124101970@163.com>
2019-12-03 13:01:49 +0800

    日志模块

---
+++ facesys-spider/src/main/java/com/ss/spider/log/constants/ModuleCode.java
@@ -0,0 +1,42 @@
+package com.ss.spider.log.constants;
+
+/**
+ * 日志模块
+ * @author 李爽超 chao
+ * @create 2019/12/03
+ * @email lishuangchao@ss-cas.com
+ **/
+public class ModuleCode {
+    /**
+     * 业务应用
+     */
+    public static final String BUSINESS = "80001";
+    /**
+     * 挂图作战
+     */
+    public static final String CHART = "80002";
+    /**
+     * 实景战法
+     */
+    public static final String SCENE = "80003";
+    /**
+     * 分析系统
+     */
+    public static final String ANALYSIS = "80004";
+    /**
+     * 布控管理
+     */
+    public static final String MONITOR = "80005";
+    /**
+     * 资源管理
+     */
+    public static final String RESOURCE = "80006";
+    /**
+     * 系统管理
+     */
+    public static final String SYSTEM = "80007";
+    /**
+     * 用户中心
+     */
+    public static final String USER = "80008";
+}
